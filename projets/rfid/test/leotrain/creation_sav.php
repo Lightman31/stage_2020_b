3cf5338a468ce3ed6dca7f56114b291cd7d2ad54
William <willyohan92@gmail.com>
2020-07-06 15:54:03 +0200

    creation good

--- projets/rfid/test/leotrain/creation_sav.php
+++ projets/rfid/test/leotrain/creation_sav.php
@@ -5,7 +5,7 @@
 		$bdd = new PDO ('mysql:host=localhost;dbname=blet;charset=utf8','root','root');
 		if ($_POST['statut21'] == "")
 		{
-			$req = $bdd->prepare('INSERT INTO SAV(num_sav,emplacement,UID) VALUES ( ?,logistique,?);'); 
+			$req = $bdd->prepare('INSERT INTO SAV(num_sav,emplacement,UID) VALUES ( ?,"logistique",?);'); 
 			$req->execute(array($_POST['num_sav4'],$_POST['UID'])) ;
 		}
 		else
@@ -13,10 +13,13 @@
 			$req = $bdd->prepare('INSERT INTO SAV(num_sav,emplacement,UID) VALUES ( ?,?,?);'); 
 			$req->execute(array($_POST['num_sav4'],$_POST['statut21'],$_POST['UID'])) ;
 		}
+		echo 'ligne ajoutée<br />	';
+		$lis = $bdd->query('SELECT * FROM SAV;');
+		while ($donnees = $lis->fetch())
+		{
+			echo $donnees['num_sav'],' ',$donnees['UID'],' ',$donnees['emplacement'] , '<br />';
+		}	
 		
-
-		$donnees = $req->fetch();	
-		echo $donnees['num_sav'];
 	?>
 </p>
 
--- projets/rfid/test/leotrain/recherche_sav_by_num_sav.php
+++ projets/rfid/test/leotrain/recherche_sav_by_num_sav.php
@@ -3,11 +3,12 @@
 	<?php 
 	// MAJ du sav a modifier
 		$bdd = new PDO ('mysql:host=localhost;dbname=blet;charset=utf8','root','root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		
 		$req = $bdd->prepare('SELECT Statut.nom_statut, Statut.inf_dat FROM SAV, Statut WHERE SAV.num_sav = ? AND SAV.id_sav = Statut.sav ORDER BY Statut.inf_dat;'); 
 		$req->execute(array($_POST['num_sav1'])) ;
+
 		$donnees = $req->fetch();	
 		echo $donnees['nom_statut'],' ', $donnees['inf_dat'];
-
 	?>
 </p>
 
