c521c6f11b2a0a8cd3fcd111de2a49456b152059
William <willyohan92@gmail.com>
2020-07-16 14:09:26 +0200

    fiche sqv

--- projets/rfid/SAVtracker/SAVTracker2.2/back/recherche_sav_by_num_sav.php
+++ projets/rfid/SAVtracker/SAVTracker2.2/back/recherche_sav_by_num_sav.php
@@ -6,10 +6,10 @@
     <link href="../bootstrap/css/tuto.css" rel="stylesheet">
 	<title>SAV</title>
 </head>
-<body>
+<body><!--
 	<p>
 
-	<?php 
+	<?php /*
 	// MAJ du sav a modifier
 	include ("connect.php");
 		$bdd = connectBDD();
@@ -42,11 +42,135 @@
 			echo $donnees['inf_dat_statut'].' : </td><td>'. $donnees['nom_statut'];
 			echo '</td></tr>';
 		}
-		echo '</table>';
+		echo '</table>';*/
 	?>
 	</p>
 
 	<p>
-	<a href="../index.php">clique ici</a> pour revenir à la page du formulaire</p>
+	<a href="../index.php">clique ici</a> pour revenir à la page du formulaire</p>-->
+
+	<h1>Num SAV <b><?php  echo $_POST['num_sav1'] ?> </b></h1>
+	<section class="row">
+		<div class ="col-md-2">
+			<?php
+
+					echo '<h3> UID : </h3>';
+					echo '<h3> Echeance :</h3>';
+			?>		
+		</div>
+		<div class ="col-md-6">
+			<?php  
+				include ("connect.php");
+				$bdd = connectBDD();
+				$data = $bdd->query('SELECT * FROM sav WHERE num_sav="'.$_POST['num_sav1'].'";');
+				if ($line_data = $data->fetch())
+				{
+					echo '<h3>';
+					echo $line_data['UID'];
+					echo '</ h3>';
+					echo '<h3>';
+					echo $line_data['echeance'];
+					echo '</ h3>';
+				}
+			?>
+		</div>
+	</section>
+	<section class="row">
+		<div class="col-xs-5">
+			
+		
+		<table class ="table table-striped">
+	<thread class="thread-dark">
+		<tr>
+			<th scope = "col">Emplacement</th>
+			<th scope = "col">Enregistrement</th>
+		</tr>
+	</thread>
+	<tbody>
+
+		<?php 
+		// MAJ du sav a modifier
+		
+		
+
+		$reponse = $bdd->query('SELECT emplacement.nom_emplacement,emplacement.inf_dat_emplacement FROM emplacement,sav WHERE emplacement.sav = (SELECT id_sav FROM sav WHERE num_sav="'.$_POST['num_sav1'].'") ORDER BY inf_dat_emplacement DESC;');
+/*
+		$query_statut = $bdd->query(' SELECT * FROM Statut where inf_dat_statut IN (SELECT MAX(inf_dat_statut) FROM Statut GROUP by sav) GROUP BY sav ;'); 
+
+		$query_emplacement = $bdd->query(' SELECT * FROM Emplacement where inf_dat_emplacement IN (SELECT MAX(inf_dat_emplacement) FROM Emplacement GROUP by sav) GROUP BY sav ;'); 
+*/
+
+		while ($emplacement = $reponse->fetch())
+		{
+			//if (($line_query_statut = $query_statut->fetch()) && ($line_query_emplacement = $query_emplacement->fetch()))
+			//{
+
+			echo '<tr><th scope = "row">';
+			echo $emplacement['nom_emplacement'];
+			echo'</td><td>';
+			echo $emplacement['inf_dat_emplacement'];
+			echo'</td><td>';
+			//echo $line_query_statut['nom_statut'].'</td><td>';
+			// echo $line_query_statut['inf_dat_statut']. '</td><td>';
+			//echo $line_query_emplacement['nom_emplacement'].'</td><td>';
+			// echo $line_query_emplacement['inf_dat_emplacement'].'</td><td>';
+			//echo $donnees['echeance'].'</td><td>';
+			//echo $donnees['UID']  ;
+
+			echo '</td></tr>';
+			//}
+		}	
+		?>
+		</tbody>
+		</table>
+		</div>
+		<div class="col-xs-5">
+			
+		
+		<table class ="table table-striped">
+	<thread class="thread-dark">
+		<tr>
+			<th scope = "col">Statut</th>
+			<th scope = "col">Enregistrement</th>
+		</tr>
+	</thread>
+	<tbody>
+
+		<?php 
+		//include ("connect.php");
+		//$bdd = connectBDD();
+		$statut = $bdd->query('SELECT statut.nom_statut,statut.inf_dat_statut FROM statut,sav WHERE statut.sav = (SELECT id_sav FROM sav WHERE num_sav="'.$_POST['num_sav1'].'") ORDER BY inf_dat_statut DESC;');
+/*
+		$query_statut = $bdd->query(' SELECT * FROM Statut where inf_dat_statut IN (SELECT MAX(inf_dat_statut) FROM Statut GROUP by sav) GROUP BY sav ;'); 
+
+		$query_emplacement = $bdd->query(' SELECT * FROM Emplacement where inf_dat_emplacement IN (SELECT MAX(inf_dat_emplacement) FROM Emplacement GROUP by sav) GROUP BY sav ;'); 
+*/
+
+		while ($line_statut = $statut->fetch())
+		{
+			//if (($line_query_statut = $query_statut->fetch()) && ($line_query_emplacement = $query_emplacement->fetch()))
+			//{
+
+			echo '<tr><th scope = "row">';
+			echo $line_statut['nom_statut'];
+			echo'</td><td>';
+			echo $line_statut['inf_dat_statut'];
+			echo'</td><td>';
+			//echo $line_query_statut['nom_statut'].'</td><td>';
+			// echo $line_query_statut['inf_dat_statut']. '</td><td>';
+			//echo $line_query_emplacement['nom_emplacement'].'</td><td>';
+			// echo $line_query_emplacement['inf_dat_emplacement'].'</td><td>';
+			//echo $donnees['echeance'].'</td><td>';
+			//echo $donnees['UID']  ;
+
+			echo '</td></tr>';
+			//}
+		}	
+		?>
+		</tbody>
+		</table>
+		</div>
+	</section>
+	
 </body>
 
